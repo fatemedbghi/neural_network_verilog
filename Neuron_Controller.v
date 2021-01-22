ebab8e298b9023e71b339b8a6fa2c284c3956a27
hamidkhd <hrkh78@gmail.com>
2021-01-22 18:16:03 +0330

    fix neuron controller.

--- Neuron_Controller.v
+++ Neuron_Controller.v
@@ -3,29 +3,31 @@
 module Neuron_Controller#(parameter N = 10)(input wire clk, rst, start, output wire [$clog2(N) - 1:0] offset, output reg ld, clr, ready);
 
     parameter [1:0] IDLE = 2'b00;
-    parameter [1:0] CALCULATION = 2'b01;
-    parameter [1:0] READY = 2'b10;
+    parameter [1:0] MLUTIPLY = 2'b01;
+    parameter [1:0] ADD = 2'b10;
+    parameter [1:0] READY = 2'b11;
 
     reg [1:0] ps, ns;
-    reg c_up, init_cnt;
-	 reg read;
+    reg c_up, init_cnt, mult_done;
     
     Counter #(.N(N)) cnt(c_up, clk, init_cnt, offset);
 
     always @(*) begin
         case (ps)
-            IDLE: ns = (start == 1) ? CALCULATION : IDLE;
-            CALCULATION: ns = (offset == N-1) ? READY : CALCULATION;
+            IDLE: ns = (start == 1) ? MLUTIPLY : IDLE;
+            MLUTIPLY: ns = (offset == N-1) ? ADD : MLUTIPLY;
+            ADD: ns = READY;
             READY: ns = IDLE;
         endcase
     end
 
     always @(*) begin
-        {c_up, init_cnt, ld, ready, clr} = 5'b00000;
+        {c_up, init_cnt, ld, ready, clr, mult_done} = 6'b000000;
 
         case (ps)
-            IDLE: {init_cnt,clr} = 2'b11;
-            CALCULATION: {c_up, ld} = 2'b11;
+            IDLE: {init_cnt, clr} = 2'b11;
+            MLUTIPLY: {c_up, ld} = 2'b11;
+            ADD: {mult_done, ld} = 2'b11;
             READY: ready = 1'b1;
         endcase
     end
